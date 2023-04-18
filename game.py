97747245de0e6df0f7193f32532126bcfcfc3abe
Wishymashy <amoralesmm@yahoo.com>
2023-04-18 14:38:52 -0500

    Set screen size and clock

--- game.py
+++ game.py
@@ -6,3 +6,21 @@ class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 600))
         self.running = True
+        self.background_color = "black"
+        self.clock = pygame.time.Clock()
+        
+        
+    def game_start(self):
+        
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    return
+                else:
+                    return
+                    
+                    
+                    
+                    
+            self.clock.tick(60)
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import pygame
 from game import Game
 
+pygame.init()
+
+
+game = Game()
+
+game.start_game()
+pygame.quit
+
+quit()
